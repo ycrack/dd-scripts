9fbb7fbd1ed57cafd46ad08ea6ba038e653f46bd
ycrack <dev.ycrack@gmail.com>
2022-02-07 15:50:33 +0900

    Update dependencies
    sift: from 0.3.5 to 0.4.3
    date-fns-tz: from 1.1.6 to 1.2.2

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * as sift from "https://deno.land/x/sift@0.3.5/mod.ts";
+export * as sift from "https://deno.land/x/sift@0.4.3/mod.ts";
 export * as GtfsRealtimeBindings from "https://esm.sh/gtfs-realtime-bindings@0.0.6";
-export * as dateFnsTz from "https://cdn.skypack.dev/date-fns-tz@1.1.6";
+export * as dateFnsTz from "https://cdn.skypack.dev/date-fns-tz@1.2.2";
