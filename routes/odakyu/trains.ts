f6a3e64fc63864142c2d8b6fed43f4da05508d8d
ycrack <dev.ycrack@gmail.com>
2023-12-11 18:19:44 +0900

    odakyu/trains: 変更に追従

--- routes/odakyu/trains.ts
+++ routes/odakyu/trains.ts
@@ -1,11 +1,12 @@
 import { json, type Handler } from "sift";
 import { gettingDataFailedResponse } from "../../utils.ts";
 
+const domain = Deno.env.get("ODAKYU_API_HOST")!;
 const apikey = Deno.env.get("ODAKYU_API_KEY")!;
 
 export const odakyuTrains: Handler = async () => {
   const res = await fetch(
-    "https://d1s70nqacx30p8.cloudfront.net/prod/v1/trains",
+    `https://${domain}/trains`,
     { headers: { "x-api-key": apikey } }
   );
 
